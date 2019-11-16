782cd9961311a9b99e5d300762b8f03861c3976a
Michelle Darcy <mdarcy137@gmail.com>
2019-11-16 02:17:13 -0800

    Switched to sdl2

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,42 @@
-extern crate minifb;
+extern crate sdl2; 
 
-use minifb::{Key, WindowOptions, Window};
-
-const WIDTH: usize = 1280;
-const HEIGHT: usize = 840;
-
-const WINDOW_START_X: usize = 8;
-const WINDOW_START_Y: usize = 8;
-
-const WINDOW_HEIGHT: usize = 800;
-const WINDOW_WIDTH: usize = 600;
-
-const WINDOW_END_X: usize = WINDOW_START_X + WINDOW_HEIGHT;
-const WINDOW_END_Y: usize = WINDOW_START_Y + WINDOW_WIDTH;
-
-fn main() {
-    println!("[DEBUG] Starting terminal...");
-
-    let mut buffer: Vec<u32> = vec![0; WIDTH * HEIGHT];
-
-    let mut window = Window::new("Test - ESC to exit",
-                                 WIDTH,
-                                 HEIGHT,
-                                 WindowOptions::default()).unwrap_or_else(|e| {
-        panic!("{}", e);
-    });
-
-    let mut x = 0;
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        for i in buffer.iter_mut() {
-            *i = x; // write something more funny here!
-            x += 1;
-            if x > 0xFFFFFF {
-                x = 0;
+use sdl2::pixels::Color;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use std::time::Duration;
+ 
+pub fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+ 
+    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+        .position_centered()
+        .build()
+        .unwrap();
+ 
+    let mut canvas = window.into_canvas().build().unwrap();
+ 
+    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    canvas.clear();
+    canvas.present();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    let mut i = 0;
+    'running: loop {
+        i = (i + 1) % 255;
+        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+        canvas.clear();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+                    break 'running
+                },
+                _ => {}
             }
         }
+        // The rest of the game loop goes here...
 
-        // We unwrap here as we want this code to exit if it fails. Real applications may want to handle this in a different way
-        window.update_with_buffer(&buffer).unwrap();
+        canvas.present();
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
-
 }
